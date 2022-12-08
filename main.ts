657391f5ac80bfb251c008a72aa1a581e47324de
Alex.C <113033697+AlexzandreChui@users.noreply.github.com>
2022-12-08 09:57:48 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,21 @@
+let distance_to_object = 0
+let strip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+strip.clear()
+strip.show()
+basic.forever(function () {
+    distance_to_object = sonar.ping(
+    DigitalPin.P1,
+    DigitalPin.P2,
+    PingUnit.Centimeters
+    )
+    basic.showNumber(distance_to_object)
+    if (distance_to_object > 4) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Green))
+    }
+    if (distance_to_object <= 4) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Blue))
+    }
+    if (distance_to_object < 1) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
+    }
+})
